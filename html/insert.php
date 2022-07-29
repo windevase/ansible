a4b5fe63017c70c35412ef5c34363880d304b530
mhan <cmhh0808@naver.com>
2022-07-29 12:21:30 +0900

    insert.php fix

--- html/insert.php
+++ html/insert.php
@@ -11,15 +11,16 @@ $mysql_charset = 'UTF8';
 $connect = new mysqli($mysql_hostname, $mysql_username, $mysql_password, $mysql_database, $mysql_port, $mysql_charset);
 
 if($connect->connect_errno){
-    echo '[잠시 후 다시 참여 부탁드립니다.] : '.$connect->connect_error.'';
+    // echo '[잠시 후 다시 참여 부탁드립니다.] : '.$connect->connect_error.'';
+    echo ("<meta http-equiv='Refresh' content='1; URL=fail.html'>");
 }else{
-    echo '[응모완료!]'.'<br>';
+    // echo '[응모완료!]'.'<br>';
 }
 
 mysqli_query($connect,"INSERT INTO user VALUES ('$_POST[user_name]', '$_POST[user_phone]', '$_POST[user_email]', now());");
 mysqli_close($connect);
 #echo ''$_POST[user_name]'<br>'
-echo ("<meta http-equiv='Refresh' content='1; URL=index.html'>");
+echo ("<meta http-equiv='Refresh' content='1; URL=done.html'>");
 ?>
 </body>
 </html>
